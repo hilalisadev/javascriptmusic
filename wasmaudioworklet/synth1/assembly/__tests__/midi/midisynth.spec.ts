224a61aff473bb60d68400d803d0c835d9fcd8c3
Peter Salomonsen <pjsalomonsen@gmail.com>
2020-07-15 09:56:36 +0200

    test noteoff

--- wasmaudioworklet/synth1/assembly/__tests__/midi/midisynth.spec.ts
+++ wasmaudioworklet/synth1/assembly/__tests__/midi/midisynth.spec.ts
@@ -1,19 +1,76 @@
 import { shortmessage, activeVoices, MidiVoice, midichannels, MidiChannel } from '../../midi/midisynth';
+import { SineOscillator } from '../../synth/sineoscillator.class';
+import { Envelope, EnvelopeState } from '../../synth/envelope.class';
+import { notefreq } from '../../synth/note';
+import { SAMPLERATE } from '../../environment';
+
+let signal: f32 = 0;
+
+class TestMidiInstrument extends MidiVoice {
+  osc: SineOscillator = new SineOscillator();
+  env: Envelope = new Envelope(0.1, 0.0, 1.0, 0.1);
+
+  noteon(note: u8, velocity: u8): void {
+    super.noteon(note, velocity);
+    this.osc.frequency = notefreq(note);
+    this.env.attack();
+  }
+
+  noteoff(): void {
+    this.env.release();
+  }
+
+  deactivateIfDone(): void {
+    if (this.env.isDone()) {
+      super.deactivateIfDone();
+    }
+  }
+
+  nextframe(): void {
+    signal += this.osc.next() * this.env.next();
+  }
+}
 
 describe("midisynth", () => {
-    it("should play note", () => {
+    it("should activate and deactivate one midivoice", () => {
       const channel = new MidiChannel();
       midichannels[0] = channel;
-      const availableVoice = new MidiVoice();
+      
+      const availableVoice = new TestMidiInstrument();
       channel.voices[0] = availableVoice;
 
       expect<MidiVoice | null>(activeVoices[0]).toBe(null, 'should be no active voices');
-      shortmessage(0x90, 64, 100);
+      shortmessage(0x90, 69, 100);
 
       expect<MidiVoice | null>(activeVoices[0]).toBe(availableVoice, 'should be one active voice');
 
-      const activeVoice: MidiVoice = activeVoices[0] as MidiVoice;
-      expect<i32>(activeVoice.note).toBe(64, "note is 64");
+      const activeVoice: TestMidiInstrument = activeVoices[0] as TestMidiInstrument;
+      expect<i32>(activeVoice.note).toBe(69, "note is 69");
       expect<i32>(activeVoice.velocity).toBe(100, "velocity is 100");
+      expect<f32>(activeVoice.osc.frequency).toBe(440, "frequency is 440");
+
+      expect<EnvelopeState>(activeVoice.env.state).toBe(EnvelopeState.ATTACK);
+      let attackFrameCount = 0;
+      while (activeVoice.env.state === EnvelopeState.ATTACK) {
+        activeVoice.nextframe();
+        attackFrameCount++;
+      }
+      expect<f32>(attackFrameCount as f32 / SAMPLERATE).toBeCloseTo(0.1 as f32);
+      expect<EnvelopeState>(activeVoice.env.state).toBe(EnvelopeState.DECAY);
+
+      // note off
+      shortmessage(0x90, 69, 0);
+      expect<EnvelopeState>(activeVoice.env.state).toBe(EnvelopeState.RELEASE);
+      let releaseFrameCount = 0;
+      while (activeVoice.env.state === EnvelopeState.RELEASE) {
+        expect<MidiVoice | null>(activeVoices[0]).toBe(availableVoice, 'voice should be active as long as it is in the release state');
+        activeVoice.nextframe();
+        activeVoice.deactivateIfDone();
+        releaseFrameCount++;
+      }
+      expect<EnvelopeState>(activeVoice.env.state).toBe(EnvelopeState.DONE);
+      expect<f32>(releaseFrameCount as f32 / SAMPLERATE).toBeCloseTo(0.1 as f32);
+      expect<i32>(activeVoice.activeVoicesIndex).toBe(-1);
+      expect<MidiVoice | null>(activeVoices[0]).toBe(null, 'should be no active voices');
     });
 });  
--- wasmaudioworklet/synth1/assembly/midi/midisynth.ts
+++ wasmaudioworklet/synth1/assembly/midi/midisynth.ts
@@ -1,5 +1,3 @@
-import { Instrument } from '../instruments/instrument.class';
-
 export const midichannels = new StaticArray<MidiChannel>(16);
 export const activeVoices = new StaticArray<MidiVoice | null>(32); // up to 32 voices playing simultaneously
 
@@ -7,6 +5,16 @@ export class MidiChannel {
     channel: u8;
     controllerValues: StaticArray<u8> = new StaticArray<u8>(128);
     voices: StaticArray<MidiVoice | null> = new StaticArray<MidiVoice | null>(16); // up to 16 voices per channel
+
+    noteoff(note: u8): void {
+        for(let n = 0; this.voices[n] !== null && n < this.voices.length; n++) {
+            const voice = this.voices[n] as MidiVoice;
+            if (voice.note === note) {
+                voice.noteoff();
+                break;
+            }
+        }
+    }
 
     activateVoice(): MidiVoice | null {
         let activeVoiceIndex: i32 = -1;
@@ -35,12 +43,43 @@ export class MidiChannel {
     }
 }
 
-export class MidiVoice {
+export abstract class MidiVoice {
     channel: MidiChannel;
     note: u8;
     velocity: u8;
     activeVoicesIndex: i32 = -1;
-    instrument: Instrument;
+
+    /**
+     * If you override this (e.g. to trigger attacks on envelopes), make sure you call super.noteon
+     * @param note 
+     * @param velocity 
+     */
+    noteon(note: u8, velocity: u8): void {
+        this.note = note;
+        this.velocity = velocity;
+    }
+
+    /**
+     * Override this to trigger releases on envelopes
+     */
+    noteoff(): void {
+
+    }
+
+    /**
+     * This will be called repeatedly as long as the voice is active
+     * 
+     * Override it to add checks for e.g. envelope to be fully released
+     */
+    deactivateIfDone(): void {
+        activeVoices[this.activeVoicesIndex] = null;
+        this.activeVoicesIndex = -1;
+    }
+
+    /**
+     * Will be called for rendering an audio frame
+     */
+    abstract nextframe(): void;
 }
 
 export function shortmessage(val1: u8, val2: u8, val3: u8): void {
@@ -51,15 +90,14 @@ export function shortmessage(val1: u8, val2: u8, val3: u8): void {
         const activatedVoice = midichannels[channel].activateVoice();
         if(activatedVoice!==null) {
             const voice = activatedVoice as MidiVoice;
-            voice.note = val2;
-            voice.velocity = val3;
+            voice.noteon(val2, val3);
         }
     } else if(
         (command === 0x80 ||
         (command === 0x90 && val3 === 0)) // 
     ) {
         // note off
-        
+        midichannels[channel].noteoff(val2);
     } else if(command === 0xb0) {
         // control change
     }
